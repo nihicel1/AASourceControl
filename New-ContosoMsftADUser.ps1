73a14151d234bd1167b41635764cc3c6d27c37f9
nihicel1 <nihicel1@hotmail.com>
2017-06-02 11:08:19 +0200

    Created New-ContosoMsftADUser.ps1 in AzureAutomation.

---
+++ New-ContosoMsftADUser.ps1
@@ -0,0 +1,41 @@
+workflow New-ContosoMsftADUser
+{
+param(
+
+[Parameter(Mandatory = $true)]
+[string]$Username,
+
+[Parameter(Mandatory = $true)]
+[string]$Password
+
+)
+
+$Server = Get-AutomationVariable -Name 'ContosoMsftDC'
+
+$PSUserCred = Get-AutomationPSCredential -Name 'Contoso_msft_student'
+
+Inlinescript{
+
+#Bring the Usernsame into the Inlinescript
+
+$Username = $using:Username
+
+#Bring the Password into the Inlinescript
+
+$Password = $using:Password
+
+#Convert the password to a secure string
+
+$SecPassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
+#Create a Contoso.msft ADUser
+
+$Newuser = new-Aduser -Name $Username -AccountPassword $SecPassword -PasswordNeverExpires $true -Enabled $true
+
+#Get the user details to pass back as Output
+
+get-ADUser -Identity $Username
+
+}-psComputerName $Server -psCredential $PSUserCred
+
+} 
